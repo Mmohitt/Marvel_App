8114bf2d730754e08ff67f885a03145fe9212bed
Mohit Kumar <mohit@flick2know.com>
2023-08-28 16:09:11 +0530

    finalizing the app and for safety purpose

--- app/src/main/java/com/example/marvel_app/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/marvel_app/viewModel/MainViewModel.kt
@@ -27,7 +27,6 @@ class MainViewModel(
             repository.getMoviesCharactersFromDB()?.let { characterList ->
                 if (characterList.isNotEmpty()) {
                     characters.postValue(characterList)
-                    Log.d("ViewModelAA","From DB")
                 } else {
                     if (isInternetAvailable(context)) {
                         try{
@@ -71,7 +70,6 @@ class MainViewModel(
                         ) + "/" + "landscape_large" + "." + it.thumbnail.extension
                     )
                 )
-
             }
         }
     }
